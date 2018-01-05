79d01a36085095eb7dda143edb3f1cf63e168231
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-01-04 21:15:14 -0800

    Add sample gulpfile for steps 1 thru 15

---
+++ sample-gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const utilities = require('gulp-util');
+const del = require('del');
+const jshint = require('gulp-jshint');
+const distProduction = utilities.env.production;
+
+const lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
